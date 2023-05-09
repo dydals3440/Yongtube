a37749f973cce23abdf161c0ca09c654008c26c1
김용민 <dydals3440@gmail.com>
2023-05-09 23:00:53 +0900

    feat: axios 실제 youtube api 통신 구현

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,7 +1,40 @@
 import axios from 'axios';
 
-export async function search(keyword) {
-  return axios
-    .get(`/videos/${keyword ? 'search' : 'popular'}.json`) //
-    .then((res) => res.data.items);
+export class Youtube {
+  constructor() {
+    this.httpClient = axios.create({
+      baseURL: 'https://www.googleapis.com/youtube/v3',
+      params: {
+        key: process.env.REACT_APP_YOUTUBE_API_KEY,
+      },
+    });
+  }
+  async search(keyword) {
+    // #은 자바스크립트에서 private 함수 클래스 내부적 호출 가능, 외부 호출 X
+    return keyword ? this.#searchByKeyword(keyword) : this.#mostPopular();
+  }
+  async #searchByKeyword(keyword) {
+    return this.httpClient
+      .get('search', {
+        params: {
+          part: 'snippet',
+          maxResults: 25,
+          type: 'video',
+          q: keyword,
+        },
+      })
+      .then((res) => res.data.items)
+      .then((items) => items.map((item) => ({ ...item, id: item.id.videoId })));
+  }
+  async #mostPopular() {
+    return this.httpClient
+      .get('videos', {
+        params: {
+          part: 'snippet',
+          maxResults: 25,
+          chart: 'mostPopular',
+        },
+      })
+      .then((res) => res.data.items);
+  }
 }
