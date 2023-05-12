ebbeca0e40574a8bd5a0ecc80be3807432d41109
김용민 <dydals3440@gmail.com>
2023-05-12 18:53:08 +0900

    feat: 상세페이지 구조

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { formatAgo } from '../utils/timeago';
+import { useNavigate } from 'react-router-dom';
 //  <VideoCard key={video.id} video={video} />
 //  videos 페이지에서 위와 같이 비디오에 관한 정보들을 네트워크 통신을 통해 받아왔다. 그 후 사용하기 위해 prop 으로 전달받아 사용한다!
 
 export default function VideoCard({ video }) {
+  const navigate = useNavigate();
   const { title, thumbnails, publishedAt, channelTitle } = video.snippet;
   return (
-    <li>
-      <img className='w-full' src={thumbnails.medium.url} alt={title} />
+    // 이미 video에 관한 정보는 prop을 통해 받아옴, 즉 해당하는 정보들도 같이 전달해야함 react router에서 부가적인 객체를 전달할떄는 2번쨰 인자에 객체를 전달하면 됨 video: video video키에 video value전달
+    <li
+      onClick={() => {
+        navigate(`videos/watch/${video.id}`, { state: { video } });
+      }}
+    >
+      <img
+        className='w-full hover:scale-125 ase-in duration-300 rounded-md'
+        src={thumbnails.medium.url}
+        alt={title}
+      />
       <div>
         <p className='font-semibold my-2 line-clamp-2'>{title}</p>
         <p className='text-sm opactiy-80'> {channelTitle}</p>
--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,5 +1,37 @@
 import React from 'react';
-
+import { useLocation } from 'react-router-dom';
+import ChannelInfo from '../components/ChannelInfo';
+import RelatedVideos from '../components/RelatedVideos';
 export default function VideoDetail() {
-  return <div>VD</div>;
+  //  video 정보를읽음
+  const {
+    state: { video },
+  } = useLocation();
+  const { title, channelId, channelTitle, description } = video.snippet;
+  console.log(video);
+  return (
+    <section>
+      <article>
+        <iframe
+          id='player'
+          type='text/html'
+          width='100%'
+          height='640'
+          src={`http://www.youtube.com/embed/${video.id}`}
+          frameborder='0'
+        />
+        <div>
+          <h2>{title}</h2>
+
+          <ChannelInfo id={channelId} name={channelTitle} />
+          {/* pre태그 여백 공백 적절히 */}
+          <pre>{description}</pre>
+        </div>
+      </article>
+      <section>
+        {/* VideoId와 연관된 비디오를 만듬. */}
+        <RelatedVideos id={video.id} />
+      </section>
+    </section>
+  );
 }
